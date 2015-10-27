7281f2009059e5437044f6d39eb392836166f87d
Lauren Trotta <lbtrotta@ufl.edu>
2015-10-27 12:20:47 -0400

    Add function for determining size class for weight

--- ts-analysis-size-class.R
+++ ts-analysis-size-class.R
@@ -2,3 +2,13 @@ get_data <- function(){
   read.csv("shrub_dimensions_labeled.csv")
   return(data)
 }
+
+get_size_class <- function(weight){
+  if(weight > 50){
+    size_class <- "large"
+  }
+  else{
+    size_class <-"small"
+  }
+  return(size_class)
+}
