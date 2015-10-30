89391323521a82aa5f6ed1010b8040157f416af7
Lauren Trotta <lbtrotta@ufl.edu>
2015-10-30 13:02:51 -0400

    Calculate GC content for dna regardless of capitalization in sequence

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,7 +1,7 @@
 ##Analysis to determine GC content of DNA in comparison to houseelf ear length
-elf_dna<-read.csv("houseelf_earlength_dna_data.csv", header=T)
+elf_data <- read.csv("houseelf_earlength_dna_data.csv", header=T)
+elf_dna <- elf_data$dnaseq
 
-##Problem 5
 
 
 
@@ -10,10 +10,10 @@ elf_dna<-read.csv("houseelf_earlength_dna_data.csv", header=T)
 install.packages("stringr")
 library("stringr")
 
-
+str_to_upper(elf_dna)
 content <- function(seq){
-  Gs <- str_count(seq, 'G')
-  Cs <- str_count(seq, 'C')
+  Gs <- str_count(str_to_lower(seq), 'g')
+  Cs <- str_count(str_to_lower(seq), 'c')
   gc_content <- (Gs + Cs) / str_length(seq) * 100 
 }
 
